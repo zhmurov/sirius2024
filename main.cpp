bbb4dd59256b3cfbff241b2fd496d3266cf85038
Artem Zhmurov <zhmurov@gmail.com>
2024-05-13 11:14:51 +0300

    Save positions into gro file

--- main.cpp
+++ main.cpp
@@ -25,6 +25,23 @@ struct float3
     float x, y, z;
 };
 
+void savePositions(const std::string filename, const std::vector<float3>& r)
+{
+    FILE* fout = fopen(filename.c_str(), "w");
+    fprintf(fout, "NaCl\n%d\n", N);
+    for (int i = 0; i < N; i++)
+    {
+        fprintf(fout, "%5d%-5s%5s%5d%8.3f%8.3f%8.3f%8.4f%8.4f%8.4f\n",
+            i, "NaCl",
+            "At", i,
+            r[i].x, r[i].y, r[i].z,
+            0.0, 0.0, 0.0
+        );
+    }
+    fprintf(fout, "%8.3f %8.3f %8.3f\n\n", L, L, L);
+    fclose(fout);
+}
+
 int main()
 {
     std::vector<float3> r(N);
@@ -36,8 +53,8 @@ int main()
         r[i].x = distributionX(randomGenerator);
         r[i].y = distributionX(randomGenerator);
         r[i].z = distributionX(randomGenerator);
-
-        std::cout << r[i].x << " " << r[i].y << " " << r[i].z << std::endl;
     }
+
+    savePositions("pos.gro", r);
 }
 
